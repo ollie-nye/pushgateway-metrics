d8bf26bfed45c60250e211789858cc85373654c5
Oliver Nye <onye@SED00404.internal.local>
2018-09-28 14:07:47 +0100

    Rubocop fixes

--- spec/pushgateway_metrics_spec.rb
+++ spec/pushgateway_metrics_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../lib/pushgateway_metrics.rb'
 
 describe PushgatewayMetrics do
@@ -16,9 +18,9 @@ describe PushgatewayMetrics do
     end
 
     it 'uses the configured instance name' do
-      expect(PushgatewayMetrics.configuration.instance_name).to eq(instance_name)
+      expect(
+        PushgatewayMetrics.configuration.instance_name
+      ).to eq(instance_name)
     end
   end
-
-  
 end
